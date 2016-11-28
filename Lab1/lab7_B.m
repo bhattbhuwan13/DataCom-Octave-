cde07caf3736ea1df1a25eb4ad6af57133e405b7
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2016-11-28 20:41:20 +0545

    Trying to solve questions 7_B of the first lab

--- Lab1/lab7_B.m
+++ Lab1/lab7_B.m
@@ -1,5 +1,10 @@
-figure
 k = linspace(-10,20,30)';
 f1 = (-0.92 * sin((0.1 * pi * k)-((3 * pi)/4)));
-stem(k,f1,'filled');
+subplot(211);
+stem(k,f1);
 title('Discrete time signal graph');
+
+k = linspace(0,50)';
+f2 = ((-0.93)^k) * exp((i*pi*k)/sqrt(350));
+subplot(212);
+stem(k,f2);
